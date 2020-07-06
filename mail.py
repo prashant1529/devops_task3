8769fee59feb507dbf9424cb8873ac2f61548900
prashant1529 <64468525+prashant1529@users.noreply.github.com>
2020-07-06 19:20:50 +0530

    Create mail.py

---
+++ mail.py
@@ -0,0 +1,14 @@
+import smtplib
+sender_email = "youremailaddress@gmail.com"
+receiver_email = "youremailaddress@gmail.com"
+password = "eL0nMu$K15"
+message = "Your code is not working correctly"
+server = smtplib.SMTP('smtp.gmail.com',587)
+server.starttls()
+server.login(sender_email,password)
+print('login successfully')
+
+server.sendmail(sender_email,receiver_email,message)
+print("Email has been sent to ",receiver_email)
+server.quit()
+© 2020 GitHub, Inc.
